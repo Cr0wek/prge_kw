d72d8a7d1e668cd5ad0050e8ebc8e9da0f1fa123
krzysztofwroniak <cr0wss@outlook.com>
2026-01-14 12:18:52 +0100

    feat: added countries layer from geoserver

--- config/node/frontend/src/components/MapComponent.js
+++ config/node/frontend/src/components/MapComponent.js
@@ -17,14 +17,24 @@ function MapComponent(props) {
         new TileLayer({
           source: new OSM(),
         }),
+        // new TileLayer({
+        //   source: new TileWMS({
+        //     url: "http://localhost:9000/geoserver/ne/wms?",
+        //     params: {
+        //       LAYERS: "ne:countries",
+        //       TILED: true,
+        //     },
+        //     serverType: "geoserver",
+        //   }),
+        // }),
         new TileLayer({
           source: new TileWMS({
-            url: "http://localhost:9000/geoserver/ne/wms?",
+            url: "http://localhost:9001/geoserver/ne/wms?",
             params: {
               LAYERS: "ne:countries",
               TILED: true,
-              serverType: "geoserver",
             },
+            serverType: "geoserver",
           }),
         }),
       ],
